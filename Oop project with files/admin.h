74bd392d7e95df5a09b0d30ac510679dc78e9f4f
Warda-1009 <w1009469@gmail.com>
2025-05-01 02:24:54 -0700

    Add files via upload

--- Oop project with files/admin.h
+++ Oop project with files/admin.h
@@ -11,13 +11,21 @@
 using namespace std;
 class admin : public user {
     election** e;
+    static int mna_count;
     int election_no;
 public:
-
-
+    admin();
+    ~admin();
     friend ostream& operator<<(ostream& out, admin& a);
-
     friend istream& operator>>(istream& in, admin& a);
     int getcnic() override;
+    void addCandidateMNA();
+    void addCandidateMPA();
+    void assignMpa();
+    bool login();
+    int check(candidate** ca,string m);
+    void createElection();
+    void CMelection();
+    void PMelection();
 };
 #endif
