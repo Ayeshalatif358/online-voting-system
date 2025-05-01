1183a1e7e5c7d2399ba7ec6eb4efb64b1e628c16
Warda-1009 <w1009469@gmail.com>
2025-05-01 02:25:45 -0700

    Add files via upload

--- Oop project with files/online voting.cpp
+++ Oop project with files/online voting.cpp
@@ -4,6 +4,8 @@
 #include"voter.h"
 #include"candidate.h"
 #include"admin.h"
+#include"local.h"
+#include"constituency.h"
 #include"election.h"
 #include"general.h"
 #include"national.h"
@@ -13,22 +15,62 @@ void elections();
 void admin_menu();
 int main()
 {
-	return 0;
+    admin a;
+    // a.createElection();
+    // election E;
+    // char ch='Y';
+    // while (ch == 'Y' || ch == 'y')
+    // {
+    //     E.addCandidate();
+    //     cout << "Enter 'Y' or 'y' to add candidate: ";
+    //     cin >> ch;
+    // }
+    // E.showCand();
+    // a.addCandidateMPA();
+    //general_election E1;
+ menu(a);
+    return 0;
 }
 void menu(admin& A)
 {
-	int choice;
-	cout << "1. Admin\n2. Candidate\n3. Voter\nEner choice";
-	cin >> choice;
-	switch (choice)
-	{
-	case 1:
-	{
-		cin >> A;
-        admin_menu();
-		break;
-	}
-	}
+    int choice;
+    cout << "1. Admin\n2. Candidate\n3. Voter\nEnter choice: ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    { int c;
+    do{
+        cout<<"1.sign up\n2.log in\n3.exit\n Enter choice: ";
+        switch (c)
+        {
+        case 1:
+        {
+            cin >> A;
+            cout<<"Now please Log in to proceed further\n";
+            break;
+        }
+        case 2:{
+          
+            if(  A.login()){
+                cout<<"login sucessfull\n";
+                admin_menu();
+                break;
+            }else{
+    cout<<"You may have entered wrong Name , NIC or Password\n";
+    break;
+            }
+        }
+        case 3:{
+cout<<"Exiting...............\n";
+        }
+        default:{
+            cout<<"Invalid input\n";
+        }
+        }
+    }while(c!=3);
+    }
+    }
 }
 void elections()
 {
